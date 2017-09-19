5a4f0db8f14eddbe2361d435583d17bc9c537e3f
SUCHETA MANDAL <sucheta.gcetts@gmail.com>
2017-09-19 15:52:39 -0700

    Update DeepLinkedListCopy.java

--- DeepLinkedListCopy.java
+++ DeepLinkedListCopy.java
@@ -27,4 +27,39 @@ public class DeepLinkedListCopy {
         }
         return nodeMap.get(head); 
     }
+    
+    public RandomList copyRandomListII(RandomList head) {
+		if (head == null) {
+			return null;
+		}
+		RandomList tmp = head;
+		while (tmp != null) {
+			RandomList copyNode = new RandomList(tmp.label);
+			copyNode.next = tmp.next;
+			tmp.next = copyNode;
+			tmp = tmp.next.next;
+		}
+
+		tmp = head;
+		while (tmp != null) {
+			RandomList oldRandom = tmp.random;
+			RandomList newNode = tmp.next;
+			if (oldRandom != null) {
+				newNode.random = oldRandom.next;
+			}
+			tmp = tmp.next.next;
+		}
+		tmp = head;
+		RandomList firstCopyNode = tmp.next;
+		while (tmp != null) {
+			RandomList nextNode = tmp.next;
+			if (tmp.next != null && tmp.next.next != null) {
+				tmp.next = tmp.next.next;
+			} else {
+				tmp.next = null;
+			}
+			tmp = nextNode;
+		}
+		return firstCopyNode;
+	}
 }
