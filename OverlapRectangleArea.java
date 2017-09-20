7188243777603cbd0fcc0ea420544d2d2561440b
SUCHETA MANDAL <sucheta.gcetts@gmail.com>
2017-09-20 13:09:25 -0700

    Update OverlapRectangleArea.java

--- OverlapRectangleArea.java
+++ OverlapRectangleArea.java
@@ -12,4 +12,17 @@ public class OverlapRectangleArea{
     int area = computeArea(l1.x,l1.y,r1.x,r1.y,l2.x,l2.y,r2.x,r2.y);
 		return ares>0;
 	}
+	
+    public bool doOverlap(Point l1, Point r1, Point l2, Point r2)
+	{
+   		 // If one rectangle is on left side of other
+    		if (l1.x > r2.x || l2.x > r1.x)
+        		return false;
+ 
+    		// If one rectangle is above other
+    		if (l1.y < r2.y || l2.y < r1.y)
+        		return false;
+ 
+    		return true;
+	}
 }
