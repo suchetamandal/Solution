0a2eda4d47345d822b32c5672558eb08c94d574f
SUCHETA MANDAL <sucheta.gcetts@gmail.com>
2017-10-03 12:36:48 -0700

    Create EncodeDecodeTinyURL.java

---
+++ EncodeDecodeTinyURL.java
@@ -0,0 +1,18 @@
+public class EncodeDecodeTinyURL {
+    Map<String,String> urlMap = new HashMap<Integer, String>();
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        String key = String.valueOf(longUrl.hashCode());
+        urlMap.put(key,longUrl);
+        return "http://tinyurl.com/"+key;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return urlMap.get(shortUrl.replace("http://tinyurl.com/",""));
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));
